e7e1461967536d8a66303cefceecc500e977c365
Renata Maraj <renatavmaraj@gmail.com>
2016-12-14 16:13:36 -0800

    Implement stack functinal

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage {};
+
+  storage.oldest= 0;
+  storage.newest = 0;
+
+  _.extend(storage, queueMethods);
+
+  return storage;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+enqueue : function(){
+  this.storage[newest] = value;
+  this.newest ++;
+},
+
+dequeue : function() {
+  var results = this.storage[oldest];
+  if(results){
+    oldest++;
+  }
+  return results;
+},
+
+size : function() {
+  return newest - oldest;
+}
+
+};
 
 
