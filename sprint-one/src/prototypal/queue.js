d1ad6e5b28ea550af085f5f6be15ca4e46317354
Renata Maraj <renatavmaraj@gmail.com>
2016-12-14 18:59:32 -0800

    Implemented stack and queue in prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  var currentKey = Object.keys(this.storage).length;
+  this.storage[currentKey] = value;
+};
+
+queueMethods.dequeue = function() {
+  var dequeued = this.storage[0];
+  var currentKey = Object.keys(this.storage).length;
+  var temp = this.storage[currentKey];
+  for (var i = 0; i < currentKey - 1; i++) {
+    this.storage[i] = this.storage[i + 1];
+  }
+  this.storage[currentKey - 1] = temp;
+  delete this.storage[currentKey - 1];
+  return dequeued;
+};
+
+queueMethods.size = function() {
+    return Object.keys(this.storage).length;
+};
 
 
