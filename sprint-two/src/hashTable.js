e8331fa1851ab53fffba1d288388f86734389e2c
Renata Maraj <renatavmaraj@gmail.com>
2016-12-16 16:22:20 -0800

    Implement set

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,7 +9,7 @@ HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   if (!bucket) { //if empty
-    this._storage.set(index, [[k,v]]); // put tuple [k, v] in bucket []
+    this._storage.set(index, [[k, v]]); // put tuple [k, v] in bucket []
   } else { //ignore else for case
     for (var i = 0; i < bucket.length; i++) {
       var tuple = bucket[i];
@@ -27,7 +27,7 @@ HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   if (!bucket) {
-      return undefined;
+    return undefined;
   }
   for (var i = 0; i < bucket.length; i++) {
     var tuple = bucket[i];//[k,v]
@@ -41,27 +41,25 @@ HashTable.prototype.retrieve = function(k) {
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  if(!bucket) {
+  if (!bucket) {
     return undefined;
   } else {
-      for(var i = 0; i < bucket.length; i++) {
-        var tuple = bucket[i];
-        //check if key in tuple matches k
-        if (tuple[0] === k) {
-          bucket.splice(i, 1);
-        }
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      //check if key in tuple matches k
+      if (tuple[0] === k) {
+        bucket.splice(i, 1);
       }
-      return undefined;
+    }
+    return undefined;
   }
-
-  //check if empty
-  //iterate through bucket and if key is in bucket, delete tuple. Check if size much smaller, resize
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ 0(n)
  */
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,43 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.length === 0) {
+    return false;
+  } else {
+    for (var i = 0; i < this._storage.length; i++) {
+      if (this._storage[i] === item) {
+        return true;
+      }
+    }
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+    if (this._storage.length === 0) {
+    return null;
+  } else {
+    for (var i = 0; i < this._storage.length; i++) {
+      if (this._storage[i] === item) {
+        this._storage.splice(i, 1);
+      }
+    }
+    return null;
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ 0(1) - constant for add method
+ 0(n) - remove and contains methods are both linear
  */
